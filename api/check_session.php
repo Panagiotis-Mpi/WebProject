9a0d4473e72d4503edc2bdc4530b134141b1e2a4
Panagiotis <106026802+Panagiotis-Mpi@users.noreply.github.com>
2025-09-16 20:58:25 +0300

    Delete api/check_session.php

--- api/check_session.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-header('Content-Type: application/json');
-if(isset($_SESSION['user_id'])){
-    echo json_encode(['loggedIn' => true, 'role' => $_SESSION['user_role'], 'name' => $_SESSION['user_name']]);
-} else {
-    echo json_encode(['loggedIn' => false]);
-}
