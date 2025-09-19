6b5faf3f7e65717fd628572cfee0d0f59649cbe6
bivol333 <triantosspiros@gmail.com>
2025-09-19 18:06:38 +0300

    Update logout.php

--- api/auth/logout.php
+++ api/auth/logout.php
@@ -2,5 +2,5 @@
 session_start();
 session_unset();
 session_destroy();
-header('Content-Type: application/json');
-echo json_encode(['success' => true]);
+header('Location: ../../views/logout.html');
+exit;
